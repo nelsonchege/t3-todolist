d920c46c3fc21170ea2b18eab3f3602b2dcac723
nelson chege <nelson3chege@gmail.com>
2023-04-26 00:29:33 +0300

    feat: create trpc routes

---
+++ src/server/api/routers/todo.ts
@@ -0,0 +1,65 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+import { todoInput } from "~/types";
+
+export const todoRouter = createTRPCRouter({
+  
+ 
+  getallTodos: protectedProcedure.query(async({ ctx }) => {
+    const todos = await ctx.prisma.todo.findMany({
+      where :{
+        userId: ctx.session.user.id
+      }
+    })
+    // return todos.map(({id,text, done})=>({id,text, done}))
+    return [{
+      id:"fake",
+      text:'fake text',
+      done:false
+    },{
+      id:"fake",
+      text:'fake text',
+      done:false
+    }]
+  }),
+
+  createTodo: protectedProcedure.input(todoInput).mutation(async({ ctx ,input}) => {
+      return ctx.prisma.todo.create({
+        data:{
+          text:input,
+          user:{
+            connect:{
+              id:ctx.session.user.id
+            }
+          }
+        }
+      })
+  }),
+
+  deleteTodo: protectedProcedure.input(z.string()).mutation(async({ ctx ,input}) => {
+    return ctx.prisma.todo.delete({
+      where:{
+        id: input
+      }
+    })
+  }),
+
+  toggle: protectedProcedure.input(z.object({
+    id:z.string(),
+    done:z.boolean()
+  })).mutation(async({ ctx ,input:{id,done}}) => {
+    return ctx.prisma.todo.update({
+      where:{
+        id: id
+      },
+      data:{
+        done
+      }
+    })
+  }),
+});
